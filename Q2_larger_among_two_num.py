438a733e1826a8e638d7e294fa42493547a5b1d0
shashankkollati <sashankkollati@gmail.com>
2024-11-08 22:12:22 +0530

    question 2 added

---
+++ Q2_larger_among_two_num.py
@@ -0,0 +1,7 @@
+n1 = int(input("Enter first number :"))
+n2 = int(input("Enter second number :"))
+
+if n1 > n2 :
+    print(f"{n1} is greater then {n2} ")
+else:
+    print(f"{n2} is greater then {n1} ")
